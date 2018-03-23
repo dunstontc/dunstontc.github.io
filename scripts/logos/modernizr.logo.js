b411eee091b58f42c73caa10337c8f9f6b70cea7
Clay Dunston <dunstontc@gmail.com>
2018-03-23 17:13:18 -0500

    Moving towards modular logos

---
+++ scripts/logos/modernizr.logo.js
@@ -0,0 +1,15 @@
+module.exports = [
+  {
+    id     : "modernizr-svg",
+    title  : "Modernizr",
+    link   : "https://modernizr.com/",
+    content: `
+      <svg width="100%" height="100%" viewBox="0 0 256 123" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
+          <g fill="#D91B77">
+              <path d="M0,122.88 L0,81.92 L40.96,81.92 L40.96,40.96 L81.92,40.96 L81.92,0 L122.88,0 L122.88,122.88 L0,122.88 Z"></path>
+              <path d="M133.12,0 C200.9856,0 256,55.0158222 256,122.878578 L133.12,122.878578 L133.12,0 L133.12,0 Z"></path>
+          </g>
+      </svg>
+    `,
+  },
+];
---
+++ scripts/logos/sails.logo.js
@@ -0,0 +1,17 @@
+module.exports = [
+  // {
+  //   id     : "sails-svg",
+  //   title  : "Sails.js",
+  //   link   : "http://sailsjs.com",
+  //   content: `
+  //     <svg width="80%" height="100%" viewBox="0 0 256 256" version="1.1"
+  //           style="transform: translate(10%, -2%);"
+  //           xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">
+  //       <g fill="#28A3B2">
+  //         <path d="M24.3404299,255.334258 C24.3404299,255.334258 -73.0918953,100.291201 127.341014,0.456539689 L127.341014,255.334258 L24.3404299,255.334258"></path>
+  //         <path d="M158.372638,255.334258 L158.372638,97.3588866 C158.372638,97.3588866 190.363104,149.565235 255.376099,255.334258 L158.372638,255.334258"></path>
+  //       </g>
+  //     </svg>
+  //   `,
+  // },
+];
